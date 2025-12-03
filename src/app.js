93df0aebf7c48e10c398e54b6e018416038387d8
Joset Santiago <josetsinph@gmail.com>
2025-12-03 17:42:42 +0000

    por terminar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-import "bootstrap";
-import "./style.css";
+function generarCarta() {
+    const numerosCarta = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const palos = ["♦", "♠", "♥", "♣"];
+
+    const numeroAleatorio = Math.floor(Math.random() * numerosCarta.length);
+    const paloAleatorio = Math.floor(Math.random() * palos.length);
+    const paloElegido = palos[paloAleatorio];
+
+    document.getElementById('cardContent').innerHTML = numerosCarta[numeroAleatorio];
+    const theCard = document.getElementById('theCard');
+    theCard.className = "";
+    theCard.classList.add("card");
+    theCard.classList.add(obtenerClasePalo(paloElegido));
+    document.querySelector('#theCard .top-izq').textContent = paloElegido;
+    document.querySelector('#theCard .bottom-der').textContent = paloElegido;
+
+}
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function () {
+    generarCarta();
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+    document.getElementById('botonGenerar').addEventListener('click', function () {
+        generarCarta();
+        clearInterval(intervalo);
+        intervalo = setInterval(generarCarta, 10000);
+
+        intervalo = setInterval(generarCarta, 10000);
+    });
+}
+
+function obtenerClasePalo(palo) {
+            if (palo === "♦") return "palo-diamantes";
+            if (palo === "♠") return "palo-picas";
+            if (palo === "♥") return "palo-corazones";
+            if (palo === "♣") return "palo-treboles";
+        }
